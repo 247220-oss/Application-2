79026296ae04ad16f79e0f404a071f2cd48ec7c8
247220-oss <247220@students.au.edu.pk>
2026-06-05 04:01:00 +0500

    Add files via upload

---
+++ UserModel.cs
@@ -0,0 +1,18 @@
+﻿// Yeh code aapne usi UserModel.cs file ke andar daalna hai
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp2
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "First name is required.")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email address is required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
